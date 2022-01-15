bc94a4b6bcdd8cdb92eea9266473f4117ece99cb
youyoung00 <you889@hanmail.net>
2022-01-15 23:39:48 +0900

    상단 TabBar Navigation 추가

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("BADUKPARK"),
+        title: const Text("BADUKPARK"),
         actions: [
           IconButton(
             icon: const Icon(
@@ -22,6 +35,20 @@ class MainPage extends StatelessWidget {
             onPressed: () {},
           )
         ],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(
+              text: '바둑리그1',
+            ),
+            Tab(
+              text: '바둑리그2',
+            ),
+            Tab(
+              text: '바둑리그3',
+            ),
+          ],
+        ),
       ),
       body: Container(),
     );
