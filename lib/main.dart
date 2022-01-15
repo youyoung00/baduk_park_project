d81da6a266961a1bd6d3d104326bbdd98edb7609
youyoung00 <you889@hanmail.net>
2022-01-16 00:57:40 +0900

    ListView 레이아웃 잡기

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
       home: const MainPage(),
     );
   }
--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -9,6 +9,11 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
   late TabController _tabController;
+  List<ListTile> listTile = [
+    ListTile(
+      title: Text('MLB'),
+    )
+  ];
 
   @override
   void initState() {
@@ -35,22 +40,71 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
             onPressed: () {},
           )
         ],
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: <Widget>[
-            Tab(
-              text: '바둑리그1',
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Material(
+              color: const Color.fromRGBO(250, 250, 250, 1.0),
+              child: TabBar(
+                unselectedLabelColor: Colors.black,
+                labelColor: Colors.deepPurple,
+                controller: _tabController,
+                tabs: const <Widget>[
+                  Tab(
+                    text: '바둑리그1',
+                  ),
+                  Tab(
+                    text: '바둑리그2',
+                  ),
+                  Tab(
+                    text: '바둑리그3',
+                  ),
+                ],
+              ),
             ),
-            Tab(
-              text: '바둑리그2',
+            const Divider(),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 32.0),
+              child: Image.network(
+                  'https://cdn.pixabay.com/photo/2016/11/20/08/33/camera-1842202__480.jpg'),
             ),
-            Tab(
-              text: '바둑리그3',
-            ),
+            const Divider(),
+            Column(
+              children: List<Widget>.generate(20, (i) {
+                return Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('KB바둑리그'),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text('만약에 신진서가 인공지능에게 이긴다면?'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text('담당자'),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text('2시간전'),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text('댓글 13'),
+                      ],
+                    ),
+                    Divider()
+                  ],
+                );
+              }).toList(),
+            )
           ],
         ),
       ),
-      body: Container(),
     );
   }
 }
