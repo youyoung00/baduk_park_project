56daca3a9a2e1906446062133516e18bb3875c93
youyoung00 <you889@hanmail.net>
2022-01-16 02:20:00 +0900

    post TabBar navigator ui 추가

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -9,18 +9,14 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
-  late TabController _tabController;
+  late TabController _topTabController;
+  late TabController _postTabController;
   int review = 0;
-  List<ListTile> listTile = [
-    ListTile(
-      title: Text('MLB'),
-    )
-  ];
-
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this);
+    _topTabController = TabController(length: 3, vsync: this);
+    _postTabController = TabController(length: 5, vsync: this);
   }
 
   @override
@@ -51,7 +47,7 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
               child: TabBar(
                 unselectedLabelColor: Colors.black,
                 labelColor: Colors.deepPurple,
-                controller: _tabController,
+                controller: _topTabController,
                 tabs: const <Widget>[
                   Tab(
                     text: '바둑리그1',
@@ -64,17 +60,17 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
                   ),
                 ],
               ),
-            ),
+            ), // 상단 탭바
             const Divider(),
             Padding(
               padding:
                   const EdgeInsets.symmetric(vertical: 8.0, horizontal: 32.0),
               child: Image.network(
                   'https://cdn.pixabay.com/photo/2016/11/20/08/33/camera-1842202__480.jpg'),
-            ),
+            ), // 광고1
             const Divider(),
             Column(
-              children: List<Widget>.generate(20, (i) {
+              children: List<Widget>.generate(30, (i) {
                 return Padding(
                   padding: const EdgeInsets.only(left: 8.0),
                   child: Column(
@@ -105,11 +101,8 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
                           ),
                           Text.rich(TextSpan(text: '댓글 ', children: [
                             TextSpan(
-                              text: '${review}',
-                              style: TextStyle(
-                                color: Colors.red
-                              )
-                            ),
+                                text: '${review}',
+                                style: TextStyle(color: Colors.red)),
                           ])),
                           // Text('댓글 13'),
                         ],
@@ -119,7 +112,63 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
                   ),
                 );
               }).toList(),
-            )
+            ),
+            Container(
+              color: const Color.fromRGBO(237, 237, 237, 1.0),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.arrow_back_ios,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Material(
+                      color: const Color.fromRGBO(237, 237, 237, 1.0),
+                      child: TabBar(
+                        indicatorColor: Colors.transparent,
+                        // automaticIndicatorColorAdjustment: false,
+                        unselectedLabelColor: Colors.black,
+                        labelColor: Colors.deepPurple,
+                        controller: _postTabController,
+                        tabs: <Widget>[
+                          Tab(
+                            text: '1',
+                          ),
+                          Tab(
+                            text: '2',
+                          ),
+                          Tab(
+                            text: '3',
+                          ),
+                          Tab(
+                            text: '4',
+                          ),
+                          Tab(
+                            text: '5',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.arrow_forward_ios,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ), // 게시글 리스트 30개 : 한페이지
           ],
         ),
       ),
